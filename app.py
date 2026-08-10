dd0c22d9b9afd3d69481cd1bf016ba3591291870
Maryem <maryemkardoudi@gmail.com>
2026-08-10 21:23:34 +0100

    Initial version of NetOpsHub

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(
+    __name__,
+    template_folder="app",
+    static_folder="app/static",
+    static_url_path="/static"
+)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
